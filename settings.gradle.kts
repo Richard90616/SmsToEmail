3398c58d30ddb9d9d7a6163a2856ffed893234a8
Richard90616 <562387883@qq.com>
2026-09-11 16:38:55 +0800

    init: settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // 备用：JavaMail for Android 镜像
+    }
+}
+
+rootProject.name = "SmsToEmail"
+include(":app")
